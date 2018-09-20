5b24f6743284a9856f0465c39028fcd037e71a45
abeck <mradamgbeck@gmail.com>
2018-09-20 12:41:04 -0400

    Pencil.write now decerements sharpness by two for capitals and one for lowercase. All Tests Pass.

--- src/main/java/Pencil.java
+++ src/main/java/Pencil.java
@@ -1,13 +1,16 @@
 public class Pencil {
     private int currentSharpness;
 
-    public Pencil(int originalSharpness){
+    public Pencil(int originalSharpness) {
         this.currentSharpness = originalSharpness;
     }
 
     public String write(String paper, String textToBeWritten) {
-        currentSharpness -= textToBeWritten.length();
-        paper = paper.concat(textToBeWritten);
+        char[] characters = textToBeWritten.toCharArray();
+        for (char character : characters) {
+            this.currentSharpness -= Character.isUpperCase(character) ? 2 : 1;
+            paper = paper.concat("" + character);
+        }
         return paper;
     }
 
